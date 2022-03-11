7c0e6a04c1d1dcff03f7e0cb017ad28817c92882
Rajath Rao <rajathrao@Rajaths-MBP.Dlink>
2022-03-11 09:41:43 +0530

    changes

--- currency_converter_main.py
+++ currency_converter_main.py
@@ -7,7 +7,6 @@ Reads the Text File and Then Converts them into Currencies and Saves the File
 '''
 import argparse
 import json
-import sys
 
 STATIC_PRICE_FILE = "current_values.json"
 
@@ -25,42 +24,38 @@ def read_json_file(file_name):
         return reader
 
 
-def main_currency_converter_function(input_value, output_currency, price_dict):
+def main_currency_converter_function(input_json, output_currency):
     '''
     This Function Validates the inputs and returns the converted currency
     '''
 
-    if (input_value["currency"] in price_dict) and not isinstance(input_value['value'], str):
+    if (input_json["currency"] in price_dict) and not isinstance(input_json['value'], str):
 
-        return static_currency_converter(input_value, output_currency, price_dict)
+        return static_currency_converter(input_json['value'], input_json['currency'], output_currency)
 
-    else:
-        # Input is Invalid
-        # print(type(input_value['value']),input_value['value'])
-        return {"value": 0, "currency": output_currency}
+    # Input is Invalid
+    # print(type(input_value['value']),input_value['value'])
+    return {"value": 0, "currency": output_currency}
 
 
-def static_currency_converter(input_value, currency, price_dict):
+def static_currency_converter(input_value, input_currency, output_currency):
     '''
-    This Function Takes in the Input Value 
-    and the Currency Type and Returns the 
+    This Function Takes in the Input Value
+    and the Currency Type and Returns the
     Coverted Currency
     '''
 
     # First Step
     # Convert Value to Euro's
 
-    if input_value['currency'] == currency:
-        return {"value": input_value['value'], "currency": currency}
+    if input_currency == output_currency:
+        return {"value": input_value, "currency": output_currency}
 
-    else:
+    price_in_euro = input_value / price_dict[input_currency]
 
-        price_in_euro = input_value["value"] / \
-            price_dict[input_value['currency']]
+    value = price_in_euro * price_dict[output_currency]
 
-        value = price_in_euro * price_dict[currency]
-
-        return {"value": value, "currency": currency}
+    return {"value": value, "currency": output_currency}
 
 
 if __name__ == '__main__':
@@ -80,4 +75,4 @@ if __name__ == '__main__':
 
     for request_values in input_file:
         print(main_currency_converter_function(
-            request_values, target_currency, price_dict))
+            request_values, target_currency))
