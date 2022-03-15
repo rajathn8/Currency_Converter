3f4d617258ca7bdf08f03b9e98640b15ccd919c5
Rajath Rao Web_soltZ <rajathn8@gmail.com>
2022-03-15 13:54:25 +0530

    added currency_converter

--- currency_converter_main.py
+++ currency_converter_main.py
@@ -3,56 +3,7 @@ Reads the Text File and Then Converts them into Currencies and Saves the File
 
 '''
 import argparse
-import json
-
-STATIC_PRICE_FILE = "current_values.json"
-
-
-def read_json_file(file_name: str) -> dict:
-    '''
-    Reading JSON File and returns the Converted Currency For Valid Entries
-
-    '''
-
-    with open(file_name, "r", encoding='UTF-8') as file:
-
-        reader = json.loads(file.read())
-
-        return reader
-
-
-def main_currency_converter_function(input_json: json, output_currency: str) -> json:
-    '''
-    This Function Validates the inputs and returns the converted currency
-    '''
-
-    if (input_json["currency"] in price_dict) and not isinstance(input_json['value'], str):
-
-        return static_currency_converter(input_json['value'], input_json['currency'], output_currency)
-
-    # Input is Invalid
-    # print(type(input_value['value']),input_value['value'])
-    return {"value": 0, "currency": output_currency}
-
-
-def static_currency_converter(input_value: int, input_currency: str, output_currency: str) -> json:
-    '''
-    This Function Takes in the Input Value
-    and the Currency Type and Returns the
-    Coverted Currency
-    '''
-
-    # First Step
-    # Convert Value to Euro's
-
-    if input_currency == output_currency:
-        return {"value": input_value, "currency": output_currency}
-
-    price_in_euro = input_value / price_dict[input_currency]
-
-    value = price_in_euro * price_dict[output_currency]
-
-    return {"value": value, "currency": output_currency}
+from src.app.services.api_services import read_json_file, currency_converter
 
 
 if __name__ == '__main__':
@@ -68,8 +19,6 @@ if __name__ == '__main__':
     input_file = read_json_file(args.filename)
     target_currency = args.target_currency
 
-    price_dict = read_json_file(STATIC_PRICE_FILE)[0]
-
     for request_values in input_file:
-        print(main_currency_converter_function(
-            request_values, target_currency))
+        print(currency_converter(
+            request_values["value"], request_values["currency"], target_currency))
