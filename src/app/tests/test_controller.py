456429d90840c5ee3d2e9d56bae0228c44d81d73
Rajath Rao Web_soltZ <rajathn8@gmail.com>
2022-03-15 18:19:12 +0530

    added tests

--- src/app/tests/test_controller.py
+++ src/app/tests/test_controller.py
@@ -2,11 +2,19 @@
 
 Tests for Controllers
 
+Note
+- Facing relative Import Errors
+
 """
+import os
+
+
 import unittest
 import logging
 from fastapi.testclient import TestClient
 from app.main import app
+
+
 
 ########## Testing the App ###############
 
@@ -46,6 +54,75 @@ class TestUserPartController(unittest.TestCase):
             "version": 2,
             "description": "Currency Converter Converts Currency"
         }
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), msg)
+        self.assertIsInstance(response.json(), dict)
+
+    def test_get_base(self):
+        """
+        test to check if base page works
+
+        """
+        response = self.client.get("/")
+
+        msg = {"Stylight": "Currency Converter Service"}
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), msg)
+        self.assertIsInstance(response.json(), dict)
+
+    def test_get_valid_conversion(self):
+        """
+        test to check if base page works
+
+        """
+        response = self.client.get(
+            "/convert/?input_value=1&input_currency=USD&output_currency=EUR")
+
+        msg = {"value": 0.913, "currency": "EUR"}
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), msg)
+        self.assertIsInstance(response.json(), dict)
+
+    def test_get_invalid_input(self):
+        """
+        test for invalid input currency
+
+        """
+        response = self.client.get(
+            "/convert/?input_value=1&input_currency=KKK&output_currency=USD")
+
+        msg = {"value": 1, "currency": "input currency not supported"}
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), msg)
+        self.assertIsInstance(response.json(), dict)
+
+    def test_get_invalid_output(self):
+        """
+        test for invalid output currency
+
+        """
+        response = self.client.get(
+            "/convert/?input_value=1&input_currency=USD&output_currency=KKK")
+
+        msg = {"value": 1, "currency": "output currency not supported"}
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), msg)
+        self.assertIsInstance(response.json(), dict)
+
+    def test_get_invalid(self):
+        """
+        test for invalid inputs
+
+        """
+        response = self.client.get(
+            "/convert/?input_value=1&input_currency=2&output_currency=3")
+
+        msg = {"result": "Invalid Input"}
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), msg)
