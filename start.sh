8312153279c523784ab21cf40bbd919710f6a9ba
Rajath Rao Web_soltZ <rajathn8@gmail.com>
2022-03-15 23:28:37 +0530

    changes to shell script

--- start.sh
+++ start.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
 nginx -g "pid $(pwd)/nginx.pid;"
-cmd ls
-yourfilenames=`ls *`
-for eachfile in $yourfilenames
-do
-   echo $eachfile
-done
 cd app/
 gunicorn --config=./gunicorn.conf.py main:app
